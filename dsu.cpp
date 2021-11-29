6eda22a8a8f8e0f4567f207917b4f339ebf66639
Nitin Gangwar <69039369+wargang@users.noreply.github.com>
2021-11-29 22:54:28 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,21 +1,38 @@
-int par[N], siz[N];
+struct DSU{
+	int connected;
+	vector<int> par, sz;
+	void setup(int n) 
+	{
+		par = sz = vector<int> (n + 1, 0);
+		for(int i = 0; i <= n; i++)
+			par[i] = i, sz[i] = 1;
+		connected = n;
+	}
+	int find(int u)
+	{
+		while(u != par[u])
+		{
+			par[u] = par[par[u]];
+			u = par[u];
+		}
+		return u;
+	}
+	int getsize(int u)
+	{
+		return sz[getPar(u)];
+	}
+	void merge(int u, int v)
+	{
+		int par1 = getPar(u), par2 = getPar(v);
+		if(par1 == par2)
+			return;
+		connected--;
+		if(sz[par1] > sz[par2])
+			swap(par1, par2);
 
-void setup(int n) {
-    for (int i = 0; i <= n ; i++) par[i] = i;
-    for (int i = 0; i <= n; i++) siz[i] = 1;
-}
-
-int find(int x) {
-    while (x != par[x])x = par[x];
-    return x;
-}
-
-bool same(int a, int b) {return find(a) == find(b);}
-
-void merge(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if (siz[a] < siz[b])swap(a, b);
-    siz[a] += siz[b];
-    par[b] = a;
-}
+		sz[par2] += sz[par1];
+		sz[par1] = 0;
+		par[par1] = par[par2];
+	}
+};
+DSU dsu;
